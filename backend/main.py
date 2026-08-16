b8c4e1558a90126303f5b70e78d6e844d8dec278
michimichimchi <m.zerner0@gmail.com>
2026-08-16 21:17:14 +0200

    first api endpoint test + frontend config

--- backend/main.py
+++ backend/main.py
@@ -3,3 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordRequestForm
 
 app = FastAPI(title="Volleyball_Dashboard", version="1.0.0")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
